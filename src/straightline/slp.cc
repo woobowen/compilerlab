9846fa4b5a2dc61e0b2e832fcc16cdd22b4e3bcb
dxy831 <2909273804@qq.com>
2026-04-16 03:20:39 +0000

    finish lab1

--- src/straightline/slp.cc
+++ src/straightline/slp.cc
@@ -5,31 +5,49 @@
 namespace A {
 int A::CompoundStm::MaxArgs() const {
   // TODO: put your code here (lab1).
-
+  // "stm1; stm2" - 递归调用stm1和stm2的MaxArgs，取最大值
+  return std::max(stm1->MaxArgs(), stm2->MaxArgs());
 }
 
 Table *A::CompoundStm::Interp(Table *t) const {
   // TODO: put your code here (lab1).
+  // "stm1; stm2"：
+  // 1. 用当前环境t解释stm1，得到新环境t1
+  Table *t1 = stm1->Interp(t);
+  // 2. 用新环境t1解释stm2，得到新环境t2
+  return stm2->Interp(t1);
 }
 
 int A::AssignStm::MaxArgs() const {
   // TODO: put your code here (lab1).
-
+  // "ID := exp" - 递归调用exp的MaxArgs
+  return exp->MaxArgs();
 }
 
 Table *A::AssignStm::Interp(Table *t) const {
   // TODO: put your code here (lab1).
-
+  // "ID := exp"：
+  // 1. 用当前环境t解释exp，得到exp的值和新环境t1
+  IntAndTable *expResult = exp->InterpExp(t);
+  // 2. 把ID和exp的值更新到环境t1中，得到新环境t2
+  return expResult->t->Update(id, expResult->i);
 }
 
 int A::PrintStm::MaxArgs() const {
   // TODO: put your code here (lab1).
-
+  // "print(expList)" : 取最大值
+  // 1. expList的NumExps() - 当前这个print语句的参数数量
+  // 2. expList的MaxArgs() - expList中print语句的最大参数数量（因为expList中可能嵌套了print语句）
+  return std::max(exps->NumExps(), exps->MaxArgs());
 }
 
 Table *A::PrintStm::Interp(Table *t) const {
   // TODO: put your code here (lab1).
-  
+  // "print(expList)" : 
+  // 1. 调用exps->Interp(t) —— 会递归求值并打印出expList中的每个exp的值并更新环境
+  IntAndTable *expListResult = exps->Interp(t);
+  // 2. 从返回的IntAndTable中取出环境
+  return expListResult->t;
 }
 
 int IdExp::MaxArgs() const {
